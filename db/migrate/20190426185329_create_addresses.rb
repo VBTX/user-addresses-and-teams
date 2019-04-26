ee3d731f3544a17825efbd8477a617f52087dff5
VBTX <jv.svedaite@gmail.com>
2019-04-26 14:03:05 -0500

    add migrations

--- db/migrate/20190426185329_create_addresses.rb
+++ db/migrate/20190426185329_create_addresses.rb
@@ -1,6 +1,12 @@
 class CreateAddresses < ActiveRecord::Migration[5.2]
   def change
     create_table :addresses do |t|
+    	t.string :street_1
+    	t.string :street_2
+    	t.string :city
+    	t.string :state
+    	t.string :zip_code
+    	t.integer :user_id
 
       t.timestamps
     end
