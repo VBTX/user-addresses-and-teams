d51a473ec0c9d98d1c291bad3021c83be06e3376
VBTX <jv.svedaite@gmail.com>
2019-04-28 13:32:14 -0500

    add address attributes setter method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
 	has_many :addresses
 	belongs_to :team
+
+	def addresses_attrubutes=(addresses_attributes)
+		addresses_attributes.each do |address_attributes|
+			self.addresses.build(address_attributes)
+		end
+	end
 end
