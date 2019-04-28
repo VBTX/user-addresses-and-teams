f78f6d8a15b4d9e23ad890d544d8e082e576567c
VBTX <jv.svedaite@gmail.com>
2019-04-28 14:58:30 -0500

    add team writer

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,18 @@ class User < ApplicationRecord
 	belongs_to :team
 
 	def addresses_attrubutes=(addresses_attributes)
-		addresses_attributes.each do |address_attributes|
+		addresses_attributes.each do |i, address_attributes|
 			self.addresses.build(address_attributes)
 		end
 	end
+
+	def team_attributes=(team_attributes)
+		self.team = Team.where(:name => team_attributes[:name]).first_or_create do
+			t.hometown = team_attributes[:hometown]
+		end
+		#team_attributes = {
+		#:name => "New Team Name",
+		#:hometown => "NYC"
+	#}
+		#create a team by name and set attributes
 end
