50755151737d678b57fb78bb45bbe6f528e9e713
VBTX <jv.svedaite@gmail.com>
2019-04-28 15:08:38 -0500

    add teams

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 class UsersController < ApplicationController
 	def new
 		@user = User.new
+		#has_many adds collection.build
 		@user.addresses.build(:address_type => "Home")
 		@user.addresses.build(:address_type => "Business")
+		@user.build_team #belongs_to adds build_association
 	end
 
 	def create
@@ -17,6 +19,6 @@ class UsersController < ApplicationController
 	private
 
 	def user_params
-		params.require(:user).permit(:email, addresses_attributes => [:street_1, :street_2, :address_type])
+		params.require(:user).permit(:email, addresses_attributes => [:street_1, :street_2, :address_type], :team_attributes => [:name, :hometown])
 	end
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,15 +1,5 @@
 class Team < ApplicationRecord
 	has_many :users
 
-	def team_name=(name)
-		#find that team by name
-	end
-
-	def new_team_attributes=(team_attributes)
-		#team_attributes = {
-		:name => "New Team Name",
-		:hometown => "NYC"
-	}
-		#create a team by name and set attributes
-	end
+	
 end
