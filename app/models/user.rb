44732ab5ae29ad1411c1715ae1b240e8b4d85551
VBTX <jv.svedaite@gmail.com>
2019-04-28 15:34:56 -0500

    add team attr

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
 	has_many :addresses
 	belongs_to :team
 
-	def addresses_attrubutes=(addresses_attributes)
+	def addresses_attributes=(addresses_attributes)
 		addresses_attributes.each do |i, address_attributes|
 			self.addresses.build(address_attributes)
 		end
@@ -12,11 +12,5 @@ class User < ApplicationRecord
 		self.team = Team.where(:name => team_attributes[:name]).first_or_create do
 			t.hometown = team_attributes[:hometown]
 		end
-
-		
-		#team_attributes = {
-		#:name => "New Team Name",
-		#:hometown => "NYC"
-	#}
-		#create a team by name and set attributes
+	end
 end
